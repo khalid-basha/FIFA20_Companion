8391bf025afe7180707a1896b0fcda6b9b0363be
yaranhasoun <78225650+yaranhasoun@users.noreply.github.com>
2022-04-01 19:02:51 +0300

    home page

---
+++ app/src/main/java/com/fifa20companion/fifa20companion/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.fifa20companion.fifa20companion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
