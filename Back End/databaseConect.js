827e85c7c57f67012c9c880700fb64e561440534
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-04-16 18:38:21 +0300

    insert into database

--- Back End/databaseConect.js
+++ Back End/databaseConect.js
@@ -20,12 +20,37 @@ console.log('MySql Connected ...');
 
 });
 const appl=express();
-appl.listen('3000',()=> {
-  console.log('server started on port 3000');
+
+// create tABLE  
+appl.get('/creattable',(req,res)=>{
+let sql =' CREATE TABLE Accounts (id int AUTO_INCREMENT ,name VARCHAR(255),password VARCHAR(255), PRIMARY KEY(id))';
+db.query(sql,(err,result)=>{
+  if (err) throw err;
+  console.log(result);
+  res.send('Accounts table created...');
+})
 
 });
 
 
+// insert 
+
+appl.get('/add0',(req,res)=>{
+let post={name:'Ahmad11',password:'123451'};
+let sql = 'INSERT INTO Accounts SET ?';
+let query =db.query(sql,post, (err,result)=>{
+  if(err) throw err ;
+  console.log(result);
+  res.send('account 1 added ...');
+     });
+});
+
+
+
+appl.listen('3000',()=> 
+  console.log('server started on port 3000');
+
+});
 
 
 //}
