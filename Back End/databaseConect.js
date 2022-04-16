6ea14c113906c2e8dc7a70610f6a8be88277fa05
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-04-16 12:23:29 +0300

    connect Database

--- Back End/databaseConect.js
+++ Back End/databaseConect.js
@@ -1,11 +1,31 @@
-exports.databas = async () => {
-    var mysql = require ('mysql');
+//exports.databas = async () => {
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername", 
-    password: "yourpassword", 
-    database: "mydb"
-    
-  });
+const express = require('express');
+const mysql =require('mysql');
+
+// Creat Connection 
+const db = mysql.createConnection ({
+  host: "localhost",
+  user: "root", 
+   password:"", 
+  database: "fifa"
+  
+});
+// conect 
+db.connect( (err)=>{
+if (err){
+  throw err;
 }
+console.log('MySql Connected ...');
+
+});
+const appl=express();
+appl.listen('3000',()=> {
+  console.log('server started on port 3000');
+
+});
+
+
+
+
+//}
