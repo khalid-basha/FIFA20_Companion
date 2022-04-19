72bcedd54eda8b5097833d93b8f4c4d1ac9e3713
Issa Abbadi <71350660+Issa-Abbadi@users.noreply.github.com>
2022-04-19 08:48:38 +0300

    Update getPlayerInfo.js

--- Back End/getPlayerInfo.js
+++ Back End/getPlayerInfo.js
@@ -1,3 +1,35 @@
 
   // this is used to get the information needed for the player 
   //(name , age ,position , price ... etc)
+
+  //  ---------each login will be inserted like this 
+
+const express = require('express');
+const mysql =require('mysql');
+// Creat Connection 
+const db = mysql.createConnection ({
+  host: "localhost",
+  user: "root", 
+   password:"", 
+  database: "fifa"  
+});
+// conect 
+db.connect( (err)=>{
+if (err){throw err;}
+console.log('MySql Connected ...');
+});
+const appl=express.Router();
+
+  // Select palyers dpending on part of their name
+  appl.get('/add2',(req,res)=>{
+    let sql = 'SELECT *FROM players WHERE Lname like "%PartOfTheName%"';
+    let query =db.query(sql,(err,results)=>{
+      if(err) throw err ;
+      console.log(results);
+      res.send('Done ...');
+         });
+    });
+
+
+
+module.exports=appl;
