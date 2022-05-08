fcf93d93fd1387bfa12eeff4f4fb2ccd8660acea
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-05-08 22:05:07 +0300

    update

--- Back End/Presentation+PresistenceLayer/teamposition.js
+++ Back End/Presentation+PresistenceLayer/teamposition.js
@@ -1,3 +1,5 @@
+const ex = require('./login');
+
 const express = require('express');
 const authenticate = require('../BusinessLayer/authenticate'); /////  1
 const db = require('../BusinessLayer/databaseConect').db;
@@ -10,7 +12,8 @@ appl.use(authenticate);///       2
 
 appl.get('/addteam',(req,res)=>{
     
-   
+   var email=ex.email;
+   console.log(email);
 
     let sql = 'SELECT * FROM teamposition WHERE email =?';
     db.query(sql,[req.session.user], function (err, result) {
